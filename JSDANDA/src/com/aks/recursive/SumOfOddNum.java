2b746e1fac0549c1bb832ee90cc254c5d7fd835d
kumar327 <adigoppulakumar@gmail.com>
2024-06-06 19:09:21 +0530

    Sum Of Odd Numbers

--- JSDANDA/src/com/aks/recursive/SumOfOddNum.java
+++ JSDANDA/src/com/aks/recursive/SumOfOddNum.java
@@ -3,7 +3,7 @@ package com.aks.recursive;
 public class SumOfOddNum {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3, 4, 4, 5, 6, 7, 4 };
+		int[] arr = { 2, 2, 3, 4, 4, 5, 6, 7, 4 };
 		int sum = sumOfOddNumber(arr, 0);
 
 		System.out.println("Number Of Times :: " + sum);
@@ -14,11 +14,12 @@ public class SumOfOddNum {
 		if (arr.length == index) {
 			return 0;
 		}
-		int sum = sumOfOddNumber(arr, index + 1);
-		if (arr[index] % 2 != 0) {
-			sum += arr[index];
-		}
+//		int sum = sumOfOddNumber(arr, index + 1);
+//		if (arr[index] % 2 != 0) {
+//			sum += arr[index];
+//		}
 
-		return sum;
+		return (arr[index] % 2 != 0 ? arr[index] : 0) + sumOfOddNumber(arr, index + 1);
+
 	}
 }
