31ddd18990d3572c4ca81af5cbe7f952a9139ad7
kumar327 <adigoppulakumar@gmail.com>
2024-06-08 19:44:53 +0530

    Sub Array Using backtracking

---
+++ JSDANDA/src/com/aks/backtracking/SubArrays.java
@@ -0,0 +1,27 @@
+package com.aks.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubArrays {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4 };
+
+		List<List<Integer>> re = new ArrayList<>();
+		backtracking(arr, re, new ArrayList<>(), 0);
+		System.out.println(re);
+	}
+
+	private static void backtracking(int[] arr, List<List<Integer>> re, ArrayList<Integer> temp, int start) {
+		re.add(new ArrayList<>(temp));
+
+		for (int i = start; i < arr.length; i++) {
+			temp.add(arr[i]);
+			backtracking(arr, re, temp, i + 1);
+			temp.remove(temp.size() - 1);
+		}
+
+	}
+
+}
