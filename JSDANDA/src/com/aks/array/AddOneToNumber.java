13c39d616fd324ed80e68755272cf3b13e23e9d8
kumar327 <adigoppulakumar@gmail.com>
2024-06-19 21:44:21 +0530

    Add one to Number

--- JSDANDA/src/com/aks/array/AddOneToNumber.java
+++ JSDANDA/src/com/aks/array/AddOneToNumber.java
@@ -1,49 +1,29 @@
 package com.aks.array;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class AddOneToNumber {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(0, 6, 0, 6, 4, 8, 8, 1));
-		System.out.println(plusOne(arr));
+		int A = 105;
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+
+		for (int i = 2; i <= A / 2; i++) {
+			if (A % i == 0) {
+				int next = A / i;
+//				list.add(i);
+//				if (list.contains(new Integer(next))) {
+//					break;
+//				} else {
+//					list.add(next);
+//				}
+			}
+			System.out.println("Number of steps :: "+i);
+		}
+		list.add(A);
+		System.out.println(list);
+		System.out.println(105 / 7);
 	}
 
-	public static ArrayList<Integer> plusOne(ArrayList<Integer> A) {
-		int rem = 1;
-		int le = A.size() - 1;
-		while (rem == 1 && le >= 0) {
-			int sum = rem + A.get(le);
-			if (sum > 9) {
-				A.set(le, 0);
-				rem = 1;
-			} else {
-				A.set(le, sum);
-				rem = 0;
-			}
-			le--;
-		}
-		if (rem == 1) {
-			ArrayList<Integer> arr = new ArrayList<>();
-			arr.add(1);
-			for (int i = 0; i < A.size(); i++) {
-				arr.add(0);
-			}
-			return arr;
-		} else {
-			boolean isZero = true;
-			int l = 0;
-			while (isZero) {
-				if (A.get(l) == 0) {
-					A.remove(l);
-				} else {
-					isZero = false;
-					break;
-				}
-				
-			}
-			return A;
-		}
-	}
 }
